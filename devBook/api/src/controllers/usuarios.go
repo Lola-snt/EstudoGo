57dc0d25b3411db0ce05500bbc019bd5eb7e7cd3
Lola-snt <lorrayne.work@gmail.com>
2024-10-03 13:59:29 -0300

    Configurando as rotas

--- devBook/api/src/controllers/usuarios.go
+++ devBook/api/src/controllers/usuarios.go
@@ -2,23 +2,23 @@ package controllers
 
 import "net/http"
 
-func CriarUsuario(w http.ResponseWriter, r http.Request) {
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Criando usuario"))
 }
-func BuscarUsuarios(w http.ResponseWriter, r http.Request) {
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("buscando todos os usuarios"))
 }
-func BuscarUsuario(w http.ResponseWriter, r http.Request) {
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("buscando um usuario"))
 }
-func AtualizarUsuario(w http.ResponseWriter, r http.Request) {
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("atualizando usuario"))
 }
-func DeletarUsuario(w http.ResponseWriter, r http.Request) {
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("deletando usuario"))
 }
--- devBook/api/src/router/rotas/rotas.go
+++ devBook/api/src/router/rotas/rotas.go
@@ -1,11 +1,24 @@
 package rotas
 
-import "net/http"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 // todas as rotas da api
 type Rota struct {
 	Uri                string
 	Metodo             string
-	Funcao             func(w http.ResponseWriter, r http.Request)
+	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
+}
+
+// configurar coloca todas as rotas dentro de router
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.Uri, rota.Funcao).Methods(rota.Metodo)
+	}
+	return r
 }
